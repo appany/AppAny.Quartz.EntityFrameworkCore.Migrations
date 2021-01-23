0458744f242e1601ddf82f114903984dca8910f3
sergeyshaykhullin <sergeyshaykhullin@gmail.com>
2021-01-23 23:53:27 +0300

    Add service containers

--- tests/AppAny.Quartz.EntityFrameworkCore.Migrations.Tests/TestSetup.cs
+++ tests/AppAny.Quartz.EntityFrameworkCore.Migrations.Tests/TestSetup.cs
@@ -2,6 +2,6 @@ namespace AppAny.Quartz.EntityFrameworkCore.Migrations.Tests
 {
 	public static class TestSetup
 	{
-		public const string ConnectionString = "host=localhost; port=5432; db=test; uid=test; pwd=test;";
+		public const string ConnectionString = "host=postgres; port=5432; db=test; uid=test; pwd=test;";
 	}
 }
