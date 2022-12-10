953b3408e167fc935d83c3f5d26bc4afa2bd35c8
Daniel Loudon <daniel_loudon@Hotmail.com>
2022-12-10 20:39:55 +0000

    Update TestSetup.cs

--- tests/AppAny.Quartz.EntityFrameworkCore.Migrations.Tests/TestSetup.cs
+++ tests/AppAny.Quartz.EntityFrameworkCore.Migrations.Tests/TestSetup.cs
@@ -3,6 +3,6 @@ namespace AppAny.Quartz.EntityFrameworkCore.Migrations.Tests
   public static class TestSetup
   {
     public const string PostgresConnectionString = "host=localhost; port=5432; db=test; uid=test; pwd=test;";
-    public const string MysqlConnectionString = "server=localhost; port=3306; database=test; uid=test; pwd=test;";
+    public const string MysqlConnectionString = "server=localhost; port=33306; database=test; uid=test; pwd=test;";
   }
 }
