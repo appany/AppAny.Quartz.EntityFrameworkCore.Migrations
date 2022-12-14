46e48f44338908672e1f837638713de0f69695af
JasonLandbridge <jasonlandbridge@protonmail.com>
2022-12-14 14:17:20 +0100

    Added integration test for SQLite which ensure the created dbcontext can be used as a JobStore

--- tests/AppAny.Quartz.EntityFrameworkCore.Migrations.Tests/SQLite/SQLiteIntegrationDbContext_IntegrationTests.cs
+++ tests/AppAny.Quartz.EntityFrameworkCore.Migrations.Tests/SQLite/SQLiteIntegrationDbContext_IntegrationTests.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Quartz;
 using Xunit;
 
 namespace AppAny.Quartz.EntityFrameworkCore.Migrations.Tests.SQLite;
@@ -8,18 +11,20 @@ namespace AppAny.Quartz.EntityFrameworkCore.Migrations.Tests.SQLite;
 public class SQLiteIntegrationDbContext_IntegrationTests : IDisposable
 {
   private readonly SQLiteIntegrationDbContext _dbContext;
+  private readonly string _connectionString;
 
   public SQLiteIntegrationDbContext_IntegrationTests()
   {
-    var connectionString = new SqliteConnectionStringBuilder()
+    // Database is created in [Root]/tests/AppAny.Quartz.EntityFrameworkCore.Migrations.Tests/bin/Debug/net6.0/
+    _connectionString = new SqliteConnectionStringBuilder()
     {
-      Mode = SqliteOpenMode.Memory,
+      Mode = SqliteOpenMode.ReadWriteCreate,
       ForeignKeys = true,
-      DataSource = "test_db",
+      DataSource = "sqlite_test.db",
       Cache = SqliteCacheMode.Shared,
     }.ToString();
 
-    var options = new DbContextOptionsBuilder<SQLiteIntegrationDbContext>().UseSqlite(connectionString).Options;
+    var options = new DbContextOptionsBuilder<SQLiteIntegrationDbContext>().UseSqlite(_connectionString).Options;
     _dbContext = new SQLiteIntegrationDbContext(options);
   }
 
@@ -31,8 +36,37 @@ public class SQLiteIntegrationDbContext_IntegrationTests : IDisposable
     Assert.Null(exception);
   }
 
+  [Fact]
+  public async Task ShouldBuildScheduler_WhenDataStoreIsSQLite()
+  {
+    // EnsureCreated is used here as Quartz does the SchemaValidation on scheduler creation
+    var exception = Record.Exception(() => _dbContext.Database.EnsureCreated());
+    Assert.Null(exception);
+
+    // Create a Quartz scheduler with the above DBContext as the JobStore
+    var scheduler = await SchedulerBuilder.Create()
+      .UseDefaultThreadPool(x => x.MaxConcurrency = 5)
+      .UsePersistentStore(x =>
+      {
+        x.PerformSchemaValidation = true;
+        x.UseMicrosoftSQLite(_connectionString);
+        x.UseJsonSerializer();
+      })
+      .BuildScheduler();
+
+    // Start Scheduler, ensure no SQLite schema validation exceptions are thrown
+    exception = await Record.ExceptionAsync(async () => await scheduler.Start());
+    Assert.Null(exception);
+
+    Assert.True(scheduler.IsStarted);
+
+    await scheduler.Shutdown();
+    Assert.True(scheduler.IsShutdown);
+  }
+
   public void Dispose()
   {
+    _dbContext.Database.EnsureDeleted();
     _dbContext.Dispose();
   }
 }
