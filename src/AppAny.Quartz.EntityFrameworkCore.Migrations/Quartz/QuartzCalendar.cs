e054e156c505eace23c53edd81577356aa8d31f8
JasonLandbridge <jasonlandbridge@protonmail.com>
2022-12-15 12:41:59 +0100

    Removed extra line

--- src/AppAny.Quartz.EntityFrameworkCore.Migrations/Quartz/QuartzCalendar.cs
+++ src/AppAny.Quartz.EntityFrameworkCore.Migrations/Quartz/QuartzCalendar.cs
@@ -2,7 +2,6 @@ namespace AppAny.Quartz.EntityFrameworkCore.Migrations
 {
   public class QuartzCalendar
   {
-
     public string SchedulerName { get; set; } = null!;
     public string CalendarName { get; set; } = null!;
     public byte[] Calendar { get; set; } = null!;
